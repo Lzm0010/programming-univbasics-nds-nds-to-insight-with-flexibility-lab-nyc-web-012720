95517ccb94a299446937a8a448ab3210981caf46
Lee McCormick <lzm0010@gmail.com>
2020-01-24 02:46:06 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -49,8 +49,8 @@ def movies_with_director_key(name, movies_collection)
   # :director_name key. This addition can be done by using the provided
   # movie_with_director_name method
   
-  updated_movies = []
-  movie_with_director_name(name, movies_collection)
+  # updated_movies = []
+  # movie_with_director_name(name, movies_collection)
 end
 
 def gross_per_studio(collection)
